2bb08bb2326e5b4e2ffa8331b42b945f40b5a5e3
Abdul-Razak <razachisibrahimah@hotmail.com>
2025-01-12 15:09:29 -0500

    Refactor to classes

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -4,49 +4,77 @@ class Product {
   description;
   price;
 
-  constructor(title, image, description, price){
+  constructor(title, image, description, price) {
     this.title = title;
     this.imageUrl = image;
     this.description = description;
     this.price = price;
-
   }
 }
 
-const productList = {
-  products: [
-    new Product("Pillow", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTEDn1_8kpGWT5c0FYXkunkX5rEqJ0k2I0cgXFruxsvnUv3F9Mm5r2lC4E&s",
-        20.46, "A soft pillow"
+
+class ProductItem {
+    constructor(product){
+        this.product = product;
+    }
+
+    addToCart() {
+        console.log('Added to cart');
+    }
+
+    renderItem(){
+        const prodEl = document.createElement("li");
+        prodEl.className = "product-item";
+        prodEl.innerHTML = `
+                <div>
+                <img  src="${ this.product.imageUrl}" alt="${ this.product.title}" >
+                <div class="product-item__content">
+                 <h2>${ this.product.title}</h2>
+                 <h3>\$${ this.product.price}</h3>
+                 <p>${ this.product.description}</p>
+                 <button>Add to Cart</button>
+                </div>
+                </div>
+              `;
+              const addCartButton = prodEl.querySelector('button');
+              addCartButton.addEventListener('click', this.addToCart.bind(this));
+              return prodEl;
+    }
+}
+
+class ProductList {
+  products = [
+    new Product(
+      "Pillow",
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTEDn1_8kpGWT5c0FYXkunkX5rEqJ0k2I0cgXFruxsvnUv3F9Mm5r2lC4E&s",
+      20.46,
+      "A soft pillow"
     ),
-    new Product("Carpet", "https://upload.wikimedia.org/wikipedia/commons/thumb/7/71/Ardabil_Carpet.jpg/397px-Ardabil_Carpet.jpg",
-        199.46, "Elegant carpet"
-    )
-  ],
+    new Product(
+      "Carpet",
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/7/71/Ardabil_Carpet.jpg/397px-Ardabil_Carpet.jpg",
+      199.46,
+      "Elegant carpet"
+    ),
+  ];
+
+  constructor() {}
 
   render() {
     const renderHook = document.getElementById("app");
     const prodList = document.createElement("ul");
     prodList.className = "product-list";
 
-    for (const prod of this.products) {
-      const prodEl = document.createElement("li");
-      prodEl.className = "product-item";
-      prodEl.innerHTML = `
-              <div>
-              <img  src="${prod.imageUrl}" alt="${prod.title}" >
-              <div class="product-item__content">
-               <h2>${prod.title}</h2>
-               <h3>\$${prod.price}</h3>
-               <p>${prod.description}</p>
-               <button>Add to Cart</button>
-              </div>
-              </div>
-            `;
-      prodList.append(prodEl);
+    for (const product of this.products) {
+      const productItem = new ProductItem(product);
+      const productElement = productItem.renderItem();
+      prodList.append(productElement);
     }
 
     renderHook.append(prodList);
-  },
-};
+  }
+}
+
+const productList = new ProductList();
 
 productList.render();
